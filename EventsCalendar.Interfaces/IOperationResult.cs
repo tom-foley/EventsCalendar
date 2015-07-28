76e3fe71410ffbf611b6f067b02a2ecdb85e0705
tom-foley <foley.236@osu.edu>
2015-07-27 21:54:18 -0400

    Removed ignored files

---
+++ EventsCalendar.Interfaces/IOperationResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsCalendar.Interfaces
+{
+    public interface IOperationResult
+    {
+        bool IsError { get; set; }
+
+        string ErrorMessage { get; set; }
+
+        string CustomMessage { get; set; }
+
+        string StackTrace { get; set; }
+
+        string InnerException { get; set; }
+
+        void Reset();
+    }
+}
---
+++ EventsCalendar.Repositories.NPoco/EventRepository_UserCode.cs
@@ -0,0 +1,30 @@
+﻿using EventsCalendar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsCalendar.Repositories.NPoco
+{
+    public partial class EventRepository
+    {
+        public IEnumerable<DateTime> GetAllEventsFromMonth(int month, int year)
+        {
+            ResetError();
+
+            List<DateTime> events = null;
+
+            try
+            {
+                events = _UnitOfWork.db.Query<DateTime>("SELECT DISTINCT(CONVERT(VARCHAR, CONVERT(DATETIME,[EventStart]),23)) AS StartDate FROM [dbo].[Event] WHERE Month([EventStart]) = @0 AND Year([EventStart]) = @1 ORDER BY StartDate ASC", month, year).ToList<DateTime>();
+            }
+            catch (Exception ex)
+            {
+                Result = new DBOperationResult(ex);
+            }
+
+            return events;
+        }
+    }
+}
